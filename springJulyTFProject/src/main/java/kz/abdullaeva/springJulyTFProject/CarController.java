6711d2645d533c566d80e4a05a5da538b8cb779d
Alexey Dartau <adartau@dar.io>
2024-07-16 11:23:32 +0500

    update add car

--- springJulyTFProject/src/main/java/kz/abdullaeva/springJulyTFProject/CarController.java
+++ springJulyTFProject/src/main/java/kz/abdullaeva/springJulyTFProject/CarController.java
@@ -1,38 +1,45 @@
 package kz.abdullaeva.springJulyTFProject;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import java.util.List;
+
 @Controller
 @RequestMapping(value = "/car")
+@RequiredArgsConstructor
 public class CarController {
-    @Autowired
-    CarRepository carRepository;
+
+    private final CarRepository carRepository;
+
     @GetMapping(value = "/home")
-    public String openHome(Model model){
+    public String openHome(Model model) {
         List<Car> cars = carRepository.findAll();
         model.addAttribute("cars", cars);
-        return"home";
+        return "home";
     }
-    @GetMapping(value = "/add-car")
-    public String openAddCar(){
-        return"add-car";
+
+    @GetMapping(value = "/addCarPage")
+    public String openAddCar(Model model) {
+        return "addCar";
     }
-    @PostMapping(value="/add-car")
+
+    @PostMapping(value = "/addCar")
     public String addCar(@RequestParam("model") String model,
                          @RequestParam("year") int year,
-                         @RequestParam("price") int price){
+                         @RequestParam("price") int price) {
         Car car = new Car();
         car.setModel(model);
         car.setYear(year);
         car.setPrice(price);
 
         carRepository.save(car);
-        return "redirect:/home";
+        return "redirect:/car/home";
     }
+
 }
